647efaf2157a7a18febe49e75338a7d8d8c3bc67
Keith Vassallo <keith@icemalta.com>
2023-07-17 12:57:19 +0100

    First commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import EmployeeCard from './EmployeeCard';
+
+// const EMPLOYEES = [
+//   { name: 'Alice Green', title: 'CEO' },
+//   { name: 'Bob Barker', title: 'CTO' },
+//   { name: 'Joe Doe', title: 'Senior Partner' },
+//   { name: 'Azat Mardan', title: 'Senior Advisor' },
+//   { name: 'Keith Vassallo', title: 'Chief Janitor' },
+// ];
+
+// const ENTRIES = [
+//   { name: "Augustus Caesar", points: 2500 },
+//   { name: "Hammurabi", points: 2250 },
+//   { name: "Abraham Lincoln", points: 2000 },
+//   { name: "Winston Churchill", points: 1900 },
+//   { name: "Nelson Mandela", points: 1800 },
+//   { name: "Catherine the Great", points: 1700 },
+//   { name: "Ashoka", points: 1600 },
+//   { name: "Marcus Aurelius", points: 1500 },
+//   { name: "Lech Wałęsa", points: 1400 },
+//   { name: "Hatsheput", points: 1300 },
+//   { name: "Charles de Gaulle", points: 1200 },
+//   { name: "Eleanor of Aquitane", points: 1100 },
+//   { name: "Ivan the Terrible", points: 1000 },
+// ];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Employees</h1>
+
+      <h1>High Scores</h1>
+    </main>
   );
 }
-
 export default App;
