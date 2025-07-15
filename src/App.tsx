22a618ecbfd269b1abbac7843dc9e5dca333b968
Keith Vassallo <keith@icemalta.com>
2025-07-15 12:56:29 +0100

    Migrated to Vite.

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ import EmployeeCard from './EmployeeCard';
 // ];
 
 // const ENTRIES = [
-//   { name: "Augustus Caesar", points: 2500 },
-//   { name: "Hammurabi", points: 2250 },
-//   { name: "Abraham Lincoln", points: 2000 },
-//   { name: "Winston Churchill", points: 1900 },
-//   { name: "Nelson Mandela", points: 1800 },
-//   { name: "Catherine the Great", points: 1700 },
-//   { name: "Ashoka", points: 1600 },
-//   { name: "Marcus Aurelius", points: 1500 },
-//   { name: "Lech Wałęsa", points: 1400 },
-//   { name: "Hatsheput", points: 1300 },
-//   { name: "Charles de Gaulle", points: 1200 },
-//   { name: "Eleanor of Aquitane", points: 1100 },
-//   { name: "Ivan the Terrible", points: 1000 },
+//   { name: 'Augustus Caesar', points: 2500 },
+//   { name: 'Hammurabi', points: 2250 },
+//   { name: 'Abraham Lincoln', points: 2000 },
+//   { name: 'Winston Churchill', points: 1900 },
+//   { name: 'Nelson Mandela', points: 1800 },
+//   { name: 'Catherine the Great', points: 1700 },
+//   { name: 'Ashoka', points: 1600 },
+//   { name: 'Marcus Aurelius', points: 1500 },
+//   { name: 'Lech Wałęsa', points: 1400 },
+//   { name: 'Hatsheput', points: 1300 },
+//   { name: 'Charles de Gaulle', points: 1200 },
+//   { name: 'Eleanor of Aquitane', points: 1100 },
+//   { name: 'Ivan the Terrible', points: 1000 },
 // ];
 
 function App() {
